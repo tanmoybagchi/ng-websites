e6623b667490fc409ff16533a0a80e3bff43427f
Tanmoy Bagchi <tanmoybagchi76@gmail.com>
2019-11-22 05:26:48 -0600

    stuff

---
+++ projects/RedditReader/src/app/listing/comment/subreddit-comment-query.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Listing, Thing } from '@app/domain/models';
+import { filter, map } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class SubredditCommentQuery {
+  constructor(private http: HttpClient) { }
+
+  execute(article: string) {
+    const url = `https://www.reddit.com/comments/${article}.json`;
+
+    const params = new HttpParams()
+      .append('limit', '10')
+      .append('depth', '3')
+      .append('raw_json', '1');
+
+    return this.http.get<Thing[]>(url, { params });
+  }
+}
